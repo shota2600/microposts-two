f7a44712be06ca8284480c0cc7087a7a16937bd8
shota.ii <shota1217+2600@gmail.com>
2022-03-06 08:04:25 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:index, :show, :create]
+  resources :users, only: [:index, :show, :new, :create]
+
+  resources :microposts, only: [:create, :destroy]
   
 end
